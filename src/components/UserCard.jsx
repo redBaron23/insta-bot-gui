a89c48575a8ece6515fff320eef35bb2b878e16b
redbaron23 <pato>
2020-08-14 17:49:10 -0300

    Favorite

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Avatar,
   Button,
@@ -9,13 +9,25 @@ import {
   CardActionArea,
   Typography
 } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import Rating from "@material-ui/lab/Rating";
+
+const StyledRating = withStyles({
+  iconFilled: {
+    color: "#ff6d75"
+  },
+  iconHover: {
+    color: "#ff3d47"
+  }
+})(Rating);
 
 const styles = {
-  root:{
-    maxWidth:350
+  root: {
+    maxWidth: 350
   },
   card: {
-    display: "flex",
+    display: "flex"
   },
   first: {
     maxWidth: 225
@@ -26,29 +38,34 @@ const styles = {
     justifyContent: "spaceBetween"
   },
   avatar: {
-    marginRight: 15
+    marginLeft:15,
+    marginRight: 25
   }
 };
 
-export default class UserCard extends Component {
-  render() {
-    const { name, src } = this.props;
-    return (
-      <Grid  item style={styles.root} xs={12} sm={3} md={3}>
-        <Card variant="outlined" style={styles.card}>
-          <CardActionArea style={styles.first}>
-            <CardContent style={styles.profile}>
-              <Avatar style={styles.avatar} alt="Juancho Tacorta" src={src} />{" "}
-              <Typography variant="body2" color="textSecondary" component="p">
-                {name}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button>Follow</Button>
-          </CardActions>
-        </Card>
-      </Grid>
-    );
-  }
+export default function UserCard(props) {
+  const { name, src } = props;
+  return (
+    <Grid item style={styles.root} xs={12} sm={3} md={3}>
+      <Card variant="outlined" style={styles.card}>
+        <CardActionArea style={styles.first}>
+          <CardContent style={styles.profile}>
+	    <StyledRating 
+              name="customized-color"
+	      max={1}
+              defaultValue={0}
+              icon={<FavoriteIcon fontSize="inherit" />}
+            />
+            <Avatar style={styles.avatar} alt="Juancho Tacorta" src={src} />{" "}
+            <Typography variant="body2" color="textSecondary" component="p">
+              {name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button>Follow</Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 }
