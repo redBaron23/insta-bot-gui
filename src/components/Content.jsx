cfe5ef3405f4630bbabba73b13f3ab81b0536ba8
redbaron23 <pato>
2020-08-14 22:15:24 -0300

    Transition remove

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -25,6 +25,16 @@ export default function Content() {
     console.log(garcas);
   }, []);
 
+  const removeElement = (userName) =>{
+    let array = garcas.filter( i => i!== userName )
+    console.log(array)
+    setGarcas(array)
+  }
+  const keepGarcas = acc => {
+    const json = JSON.stringify(acc);
+    console.log(json)
+    localStorage.setItem("garcas", acc);
+  };
   const loadGarcas = () => {
     const data = localStorage.getItem("account");
     const json = JSON.parse(data);
@@ -43,9 +53,11 @@ export default function Content() {
   return (
     <Grid container styles={classes.root} spacing={2}>
       <h1>Total: {garcas.length}</h1>
+      {keepGarcas(garcas)}
       {garcas.map(i => (
         <UserCard
           userName={i}
+	  removeElement={removeElement}
           src="https://happytravel.viajes/wp-content/uploads/2020/04/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
         />
       ))}
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -49,9 +49,10 @@ const styles = {
 };
 
 export default function UserCard(props) {
-  const { userName } = props;
+  const { userName, removeElement } = props;
   const [src, setSrc] = useState(false);
-  const [show,setShow] = useState(true);
+  const [show, setShow] = useState(true);
+  const [remove, setRemove] = useState(false);
   const [buttonText, setButtonText] = useState("Unfollow");
   const [buttonDisabled, setButtonDisabled] = useState(false);
   useEffect(() => {
@@ -66,19 +67,13 @@ export default function UserCard(props) {
     account.import(json);
 
     if (buttonText === "Unfollow") {
-      const garcas = localStorage.getItem("garcas");
-      const garcasParsed = JSON.parse(garcas);
       newText = "Follow";
-      setShow(false)
-   /*   account.unfollow(userName)
-        .then(setButtonDisabled(false))
-        .then(garcasParsed.filter(i => i !== userName))
-        .then(i => JSON.stringify(i))
-        .then(i => localStorage.setItem("garcas", i))
-	.then(setShow(false))
-
+      account
+        .unfollow(userName)
+        .then(bool => setShow(!bool))
+        .then(removeElement(userName))
         .catch(e => console.log(e));
-	*/} else {
+    } else {
       newText = "Unfollow";
       account.follow(userName).then(setButtonDisabled(false));
     }
@@ -95,42 +90,52 @@ export default function UserCard(props) {
       .catch(e => setSrc(defaultSrc));
   };
   return (
-    <Zoom in={show} style={{ transitionDelay: show ? "500ms" : "0ms" }}>
-      <Grid item style={styles.root} xs={12} sm={3} md={3}>
-        <Card variant="outlined" style={styles.card}>
-          <div style={styles.first}>
-            <CardContent style={styles.profile}>
-              <StyledRating
-                name="customized-color"
-                max={1}
-                defaultValue={0}
-                icon={<FavoriteIcon fontSize="inherit" />}
-              />
-              <CardActionArea
-                style={styles.profile}
-                onClick={() =>
-                  window.open("https://instagram.com/" + userName, "_blank")
-                }
+    <Zoom in={show} onExited={ () => setRemove(true) } style={{ transitionDelay: show ? "800ms" : "0ms" }}>
+      <div style={{display: remove ? "none" : "" }}>
+        <Grid item style={styles.root} xs={12} sm={3} md={3}>
+          <Card variant="outlined" style={styles.card}>
+            <div style={styles.first}>
+              <CardContent style={styles.profile}>
+                <StyledRating
+                  name="customized-color"
+                  max={1}
+                  defaultValue={0}
+                  icon={<FavoriteIcon fontSize="inherit" />}
+                />
+                <CardActionArea
+                  style={styles.profile}
+                  onClick={() =>
+                    window.open("https://instagram.com/" + userName, "_blank")
+                  }
+                >
+                  <Avatar
+                    style={styles.avatar}
+                    alt="Juancho Tacorta"
+                    src={src}
+                  />{" "}
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {userName}
+                  </Typography>
+                </CardActionArea>
+              </CardContent>
+            </div>
+            <CardActions>
+              <Button
+                variant="outlined"
+                disabled={buttonDisabled}
+                color="secondary"
+                onClick={buttonHandle}
               >
-                <Avatar style={styles.avatar} alt="Juancho Tacorta" src={src} />{" "}
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {userName}
-                </Typography>
-              </CardActionArea>
-            </CardContent>
-          </div>
-          <CardActions>
-            <Button
-              variant="outlined"
-              disabled={buttonDisabled}
-              color="secondary"
-              onClick={buttonHandle}
-            >
-              {buttonText}
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
+                {buttonText}
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </div>
     </Zoom>
   );
 }
