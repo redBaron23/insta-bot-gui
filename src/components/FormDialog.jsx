165df59c104532016d84aea1994a71dda8b9f3ab
redbaron23 <pato>
2020-09-15 02:20:15 -0300

    Dialog

---
+++ src/components/FormDialog.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function FormDialog(props) {
+
+
+  const { show } = props;
+  const [open, setOpen] = React.useState(show);
+
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Codigo de verificacion</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+	    Los servidores estan en Virginia, entonces Instagram enviaria un codigo via SMS/Email
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="code"
+            label="Code"
+            type="code"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Send
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
