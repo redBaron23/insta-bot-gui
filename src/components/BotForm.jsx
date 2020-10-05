bf2eb2dd1fbe657064bad2dc94c77ccdd2596390
redbaron23 <pato>
2020-10-05 15:28:18 -0300

    Un avance ahora me voy a otra cosa mariposa

---
+++ src/components/BotForm.jsx
@@ -0,0 +1,233 @@
+import React, { useState } from "react";
+import Proptypes from "prop-types";
+import Avatar from "@material-ui/core/Avatar";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import { FaPowerOff } from "react-icons/fa"
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import UserCard from "./UserCard";
+import FormDialog from "./FormDialog";
+
+import { Account } from "../fun/account";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/redbaron23/insta-bot-gui">
+        redbaron23
+      </Link>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+const styles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+export default function BotForm(props) {
+  const { onLogin } = props;
+  const classes = styles();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [usernameError, setUsernameError] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [openNotification, setOpenNotification] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [notificationSeverity, setNotificationSeverity] = useState("");
+
+  const handleClick = () => {
+    setOpenNotification(true);
+  };
+
+  const handleCloseForm = () => {
+    setOpenForm(false);
+  };
+
+  const handleCloseNotification = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenNotification(false);
+  };
+
+  const handleCredentials = e => {
+    const { name, value } = e.target;
+    name === "username" ? setUsername(value) : setPassword(value);
+  };
+  const customAlert = (message, type) => {
+    setNotificationMessage(message);
+    setNotificationSeverity(type);
+    setOpenNotification(true);
+  };
+  const handleSubmit = e => {
+    if (!loading) {
+      setLoading(true);
+      let status = true;
+      //No refresh
+      e.preventDefault();
+      let account = { username, password };
+      if (account.username && account.password) {
+        status = isMatch(account);
+      } else {
+        let message = "No username or password detected";
+        !account.username ? setUsernameError(true) : setUsernameError(false);
+        !account.password ? setPasswordError(true) : setPasswordError(false);
+
+        customAlert(message, "error");
+      }
+    }
+  };
+
+  const handleSendCode = code => {
+    const userName = localStorage.getItem("userName");
+    const password = localStorage.getItem("password");
+    let account = new Account(userName, password);
+    account
+      .initCode(code)
+      .then(res => keepAccount(res, customAlert, username))
+      .catch(e => console.log(e));
+  };
+  const keepAccount = async (data, customAlert, userName) => {
+    if (data === 402) {
+      //Necesito codigo de verificacion
+      setOpenForm(true);
+      console.log("Dame el codigo paper");
+    } else if (data === 401) {
+      customAlert("Wrong password or username", "error");
+    } else {
+      console.log("RESPUESTA", data);
+      localStorage.setItem("account", JSON.stringify(data));
+      sessionStorage.setItem("userName", userName);
+      //Logged
+      onLogin();
+    }
+    setLoading(false);
+  };
+  const isMatch = async acc => {
+    //Send to backend
+    let backend = true;
+
+    //LOGEADO
+    localStorage.setItem("userName", acc.username);
+    localStorage.setItem("password", acc.password);
+    let account = new Account(username, password);
+    try {
+      let res = await account.init();
+      keepAccount(res.data, customAlert, account.userName);
+      res.status !== 200 && console.log("El error", res);
+    } catch (e) {
+      console.log("El err", e);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <FormDialog
+        show={openForm}
+        onSend={handleSendCode}
+        onClose={handleCloseForm}
+      />
+      <Snackbar
+        open={openNotification}
+        autoHideDuration={6000}
+        onClose={handleCloseNotification}
+      >
+        <Alert
+          onClose={handleCloseNotification}
+          severity={notificationSeverity}
+        >
+          {notificationMessage}
+        </Alert>
+      </Snackbar>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <FaPowerOff />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log in
+        </Typography>
+        <form className={classes.form} noValidate>
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            error={usernameError}
+            autoComplete="username"
+            onChange={handleCredentials}
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            error={passwordError}
+            onChange={handleCredentials}
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={username.length < 6 || password.length < 6 || loading}
+            onClick={handleSubmit}
+            className={classes.submit}
+          >
+            {"Log"}
+            {loading && (
+              <CircularProgress size={24} className={classes.buttonProgress} />
+            )}
+            {" In"}
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
--- src/components/ContentBot.jsx
+++ src/components/ContentBot.jsx
@@ -4,6 +4,10 @@ import { Grid, Button, Box, Typography } from "@material-ui/core";
 import UserCard from "./UserCard";
 import { Account } from "../fun/account";
 import { sleep } from "../fun/helper";
+import BotForm from "./BotForm"
+
+
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,100 +29,10 @@ export default function ContentGarcas() {
   const [removePointer, setRemovePointer] = useState(0);
   const [buttonText, setButtonText] = useState("Unfollow All");
 
-  useEffect(() => {}, []);
-  const removeElement = userName => {
-    setGarcas(oldGarcas => oldGarcas.filter(i => i.userName !== userName));
-  };
-  const handleButton = () => {
-    setLoading(true);
-    let i = 0 + removePointer;
-    const data = localStorage.getItem("account");
-    const json = JSON.parse(data);
-    const account = new Account(
-      localStorage.getItem("userName"),
-      localStorage.getItem("password")
-    );
-    account.import(json);
-    if (garcas[i]) {
-      unfollowButton(account, i);
-      setRemovePointer(removePointerOld => removePointerOld + 10);
-    }
-  };
-  const unfollowButton = (account, i) => {
-    let userNames, users;
-    const data = localStorage.getItem("likes");
-    const likes = JSON.parse(data);
-
-    users = garcas.filter(i => !likes.includes(i.userName));
-
-    console.log("Going to unfollow");
-    console.log("Longitud de garcas", garcas.length);
-    console.log("Longitud de users", users.length);
-
-    userNames = users.map(i => i.userName);
-    setLoading(true);
-    //Dynamic es para que solo los deje de seguir
-    console.log("El nombre es", account.userName);
-    if (buttonText.includes("Stop")) {
-      //Lo paras
-      account.stopBot().then(res => {
-        setLoading(false);
-        setButtonText("Unfollow All");
-      });
-    } else {
-      //Lo activas
-      account.startBot("dynamic", userNames).then(res => {
-        setLoading(false);
-        setButtonText("Stop unfollow All");
-      });
-    }
-  };
-
-  const updateButton = async () => {
-    const data = localStorage.getItem("account");
-    const json = JSON.parse(data);
-    const account = new Account(
-      json.userName,
-      localStorage.getItem("password")
-    );
-    console.log("Going to update");
-  };
-  const loadGarcas = async () => {
-    let json, garcas;
-    const data = localStorage.getItem("account");
-    const cookies = JSON.parse(data);
-    const userName = localStorage.getItem("userName");
-    console.log("Mi datita", userName);
-    const account = new Account(userName, localStorage.getItem("password"));
-    //TEST
-    account.import(cookies);
-    try {
-      garcas = await account.getGarcas();
-      json = garcas.map((i, index) => {
-        return { index: index, userName: i, alive: true, like: false };
-      });
-      await setGarcas(json);
-      setTotalUnfollowers(json.filter(x => x.alive).length);
-    } catch (e) {
-      console.log("Hubo un error en loadGarcas intentando en 30sec", e);
-      await sleep(30 * 1000);
-      await loadGarcas();
-    } /*   account
-      .getGarcas()
-      .then(usernames =>
-        usernames.map((i, index) => {
-          let json = { index: index, userName: i, alive: true, like: false };
-          return json;
-        })
-      )
-      .then(json => setGarcas(json))
-      .catch(e => console.log(e));*/
-  };
   return (
     <Grid container spacing={2}>
       <div styles={classes.root}>
-
-
+	<BotForm />
       </div>
     </Grid>
   );
